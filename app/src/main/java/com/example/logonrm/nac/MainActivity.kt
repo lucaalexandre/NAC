3a92408ce07b1d1601e91a1934cd8b0459f05a94
logonrm <logonrm@fiap.com.br>
2018-05-18 09:37:28 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/nac/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.logonrm.nac
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+}
